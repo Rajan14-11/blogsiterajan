23c291d83c90582734ce67f410d4886e2f2da1ae
Rajan14-11 <90331294+Rajan14-11@users.noreply.github.com>
2021-11-21 12:16:21 +0530

    Add files via upload

---
+++ blog website/app.js
@@ -0,0 +1,17 @@
+const reply = (res)=>{
+let res = document.getElementById(res)
+console.log(res)
+}
+
+const replies = document.querySelector('.left_div__reply')
+replies.addEventListener('click',()=>{
+   let replie = document.querySelector('#reply1')
+   replie.classList.(replies)
+})
+
+
+if(){}
+else if (condition) {
+    
+}
+
